bfd52021dfd5d22e3f14083914e16bfcb72c2aa8
Shitanshu <shitanshukumar639@gmail.com>
2024-06-27 11:40:07 +0530

    Complteted first lecture

---
+++ lecture1.py
@@ -0,0 +1,60 @@
+# Lecture1
+
+
+## Problem 1
+## Write a program to input 2 numbers & print their sum
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# sum = num1 + num2
+
+# print("Sum =" , sum)
+
+
+
+
+
+
+
+
+## Problem 2
+## WAP to input side of a square & print their area
+
+
+# side = float(input("Enter square side: "))
+# area = side * side
+
+# print("Area =" , area)
+
+
+
+
+
+
+## Problem 3
+## WAP to input 2 floating mumbers & print their average
+
+
+# float1 = float(input("Enter first floating number : "))
+# float2 = float(input("Enter second floating number : "))
+# average = (float1 + float2)/2
+
+# print("Average = " , average)/
+
+
+
+
+
+
+
+
+## Problem 4
+## WAP to input 2 int numbers and print true if a is greater than or equal to b if not print false
+
+
+a = float(input("a : "))
+b = float(input("b : "))
+
+
+print( a >= b )
